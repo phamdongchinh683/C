e997c02085da5ee6e6d8ade33bab9dd5648efe85
Chinh <chinh99501@donga.edu.vn>
2024-01-23 01:05:03 +0700

    if else

--- Learn C/main.c
+++ Learn C/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if  (number%2 == 0) {
+        printf("%d is an even integer.",number);
+    }
+    else {
+        printf("%d is an odd integer.",number);
+    }
+
     return 0;
 }
