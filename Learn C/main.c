872957c33cd3f1864eda6403184186d4650538f1
Chinh <chinh99501@donga.edu.vn>
2024-02-02 11:12:54 +0700

    function in c

--- Learn C/main.c
+++ Learn C/main.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+int getInteger(); // function prototype
 
-int total(int a, int b);
+int main()
+{
 
-int main() {
-    int sum = total(2,2);
-    printf("%d",sum);
+    int n, i, flag = 0;
+    // n = function getInteger
+    n = getInteger();
+
+    if (n == 0 || n == 1)
+        flag = 1;
+    for (i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    // check
+    if (flag == 1)
+        printf("%d is not a prime number.", n);
+    else
+        printf("%d is a prime number.", n);
+
     return 0;
 }
 
-int total(int a, int b){
-        int total = a + b;
-    return total;
+// input n from function getInteger
+// function definition
+int getInteger()
+{
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    return n;
 }
